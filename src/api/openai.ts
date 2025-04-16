7dca268284eec1d5f776f4f642e596696cdd5e9b
Dusan Vulic <dvulic996@gmail.com>
2025-04-16 14:39:59 +0200

    remove logs and delete unused component

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -3,8 +3,6 @@ import axios from 'axios'
 const apiUrl = import.meta.env.VITE_OPENAI_API_URL
 const apiKey = import.meta.env.VITE_OPENAI_API_KEY
 
-console.log({ apiKey, apiUrl })
-
 export const openai = axios.create({
 	baseURL: apiUrl,
 	headers: {
